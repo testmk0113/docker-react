9d0b2144a9a6c3f34d6f93a10819bc9df7eb81aa
Mohit Kalia <mohit.kalia@clover.com>
2022-08-30 14:02:18 -0400

    changed text to feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello there!
         </a>
       </header>
     </div>
