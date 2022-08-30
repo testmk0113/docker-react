e166420e993fd2ab929f020f419ea46c458adb89
Mohit Kalia <mohit.kalia@clover.com>
2022-08-30 14:08:21 -0400

    changed text in the feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          Hi there testing the feature branch!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello there!
+          Learn React
         </a>
       </header>
     </div>
